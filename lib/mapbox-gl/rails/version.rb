7944d581270494fbfc6b3bd678e6b892633428cb
Nikita Bulai <bulaj.nikita@gmail.com>
2022-02-12 13:23:53 +0300

    Release 2.7.0 with updated plugins :tada:

--- lib/mapbox-gl/rails/version.rb
+++ lib/mapbox-gl/rails/version.rb
@@ -14,9 +14,9 @@ module MapboxGl
       # Major version number
       MAJOR = 2
       # Minor version number
-      MINOR = 6
+      MINOR = 7
       # Smallest version number
-      TINY = 1
+      TINY = 0
 
       # Full version number
       STRING = [MAJOR, MINOR, TINY].compact.join('.')
